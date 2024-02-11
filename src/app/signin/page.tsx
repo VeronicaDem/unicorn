fbb98c18f645f3684735ea3ca2d890f802f5bb24
VeDemchenko <VeDemchenko@croc.ru>
2024-02-11 20:29:46 +0300

    feat(*): add react-hook-form

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,7 +2,10 @@
  * Страница логина
  * Перенаправляет при успешном входе в систему на главную страницу
  */
-'use client'
+"use client";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { cookies } from "next/headers";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -12,68 +15,99 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-
+import axios from "axios";
+import Cookies from "universal-cookie";
 export default function SignIn() {
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
-    };
+  const router = useRouter();
+  const methods = useForm();
 
-    return (
-        <Container component="main" maxWidth="sm">
-            <Box
-                sx={{
-                    boxShadow: 3,
-                    borderRadius: 2,
-                    px: 4,
-                    py: 6,
-                    marginTop: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
+  const { control, handleSubmit } = methods;
+  const login = (data: any) => {
+    console.log("data: ", data);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    axios
+      .post("http://localhost:3000/api/user/login", {
+        data: {
+          username: data.get("email"),
+          password: data.get("password"),
+        },
+      })
+      .then((res) => {
+        console.log("Successfully logged in");
+        const cookies = new Cookies(null, { path: "/" });
+        cookies.set("userId", res.data.userId);
+        cookies.set("username", res.data.username);
+        cookies.set("accessToken", res.data.accessToken);
+        cookies.set("refreshToken", res.data.refreshToken);
+        cookies.set("accessTokenExpiresAt", res.data.accessTokenExpiresAt);
+        cookies.set("refreshTokenExpiresAt", res.data.refreshTokenExpiresAt);
+        router.push("/");
+      })
+      .catch((err) => {
+        console.log("Error while log in", err);
+      });
+  };
 
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign In
-                    </Button>
+  return (
+    <FormProvider {...methods}>
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            boxShadow: 3,
+            borderRadius: 2,
+            px: 4,
+            py: 6,
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                autoFocus
+                value={value}
+                onChange={onChange}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                type="password"
+                value={value}
+                onChange={onChange}
+              />
+            )}
+          />
 
-                </Box>
-            </Box>
-        </Container>
-    );
+          <Button
+            onClick={handleSubmit(login)}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Container>
+    </FormProvider>
+  );
 }
-
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,29 +3,37 @@ import isAuth from "./features/auth";
 import { Roles } from "./features/auth/";
 
 export default async function middleware(request: NextRequest) {
-  if (!isAuth(request)) {
-    return NextResponse.redirect(new URL("/signin", request.nextUrl.origin));
-  }
-  if (
-    !request.nextUrl.pathname.startsWith("/forbidden") &&
-    !request.nextUrl.pathname.startsWith("/signin")
-  ) {
-    return NextResponse.next();
-  }
-  return await Roles.isResolved(request)
-    .then((isResolved) => {
-      if (!isResolved) {
-        return NextResponse.redirect(
-          new URL("/forbidden", request.nextUrl.origin)
-        );
-      } else {
-        return NextResponse.next();
+  return isAuth(request)
+    .then((authUser) => {
+      if (
+        !request.nextUrl.pathname.startsWith("/forbidden") &&
+        !request.nextUrl.pathname.startsWith("/signin")
+      ) {
+        if (!authUser) {
+          return NextResponse.redirect(
+            new URL("/signin", request.nextUrl.origin)
+          );
+        }
+        return Roles.isResolved(request)
+          .then((isResolved) => {
+            if (!isResolved) {
+              return NextResponse.redirect(
+                new URL("/forbidden", request.nextUrl.origin)
+              );
+            } else {
+              return NextResponse.next();
+            }
+          })
+          .catch((err) => {
+            console.log("Error. Redirect to forbidden page", err);
+            return NextResponse.redirect(
+              new URL("/forbidden", request.nextUrl.origin)
+            );
+          });
       }
+      return NextResponse.next();
     })
     .catch((err) => {
-      console.log("Error. Redirect to forbidden page", err);
-      return NextResponse.redirect(
-        new URL("/forbidden", request.nextUrl.origin)
-      );
+      return NextResponse.redirect(new URL("/signin", request.nextUrl.origin));
     });
 }
